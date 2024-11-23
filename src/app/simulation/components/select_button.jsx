fd725398261424966ffa2cca926c7074e38a1063
Ippei Kanetsuki <one.flat0402@gmail.com>
2024-11-23 20:28:11 +0900

    RFP案件。ボタン要素をコンポーネントに分解し、それに合わせてcssの設定と受注案件のメインページを更新

---
+++ src/app/simulation/components/select_button.jsx
@@ -0,0 +1,21 @@
+// 選択ボタンのコンポーネント
+'use client';
+
+import React from 'react';
+
+const SelectButton = ({label, index, selectedIndex, onSelect}) => {
+    return (
+        <div className="my-1 text-xs">
+            <button
+                    // 親コンポーネントから渡されるselectedIndex中にボタンのindexが含まれていれば選択状態のCSSを適用
+                    className={`app-selectable-button ${selectedIndex.includes(index) ? 'selected' : ''}`}
+                    // クリックしたら親コンポーネントで指定して渡されている関数onSelectを実行
+                    onClick={() => onSelect(index)}
+                >
+                    {label}
+                </button>
+        </div>
+    );
+}
+
+export default SelectButton;
--- src/app/simulation/page.jsx
+++ src/app/simulation/page.jsx
@@ -1,110 +1,115 @@
 'use client';
 
 import React, { useState } from 'react';
+import SelectArea from "./components/select_area.jsx";
+import MultipleSelectArea from './components/multiple_select_area.jsx';
 
 export default function Home() {
-    const [selected, setSelected] = useState(null); // ボタンの選択状態を管理
     const [year, setYear] = useState(""); // 年の選択状態
     const [month, setMonth] = useState(""); // 月の選択状態
     const [day, setDay] = useState(""); // 日の選択状態
 
-    const handleSelect = (id) => {
-        setSelected(id); // 選択されたボタンのIDを設定
-    };
+    // ボタンの項目リスト
+    const family_member = ["ご本人", "ご夫婦", "ご夫婦+お子様1人", "ご夫婦+お子様2人以上", "親子", "ご本人+ご両親"]
+    const life_plan = ["旅行", "趣味", "車", "生涯学習", "こどもの教育費", "こども・孫への援助", "仕事・ボランティア", "住宅購入・リフォーム"]
+    const savings = ["300万円", "500万円", "800万円", "1,000万円", "2,000万円", "3,000万円", "5,000万円", "5,000万円以上"]
+    const annual_income = ["0万円", "100万円","300万円", "500万円", "800万円", "1,000万円", "2,000万円", "3,000万円以上"]
+    const annual_income_spouse = ["0万円", "100万円","300万円", "500万円", "800万円", "1,000万円", "2,000万円", "3,000万円以上"]
+    const monthly_consumption = ["10万円", "20万円", "30万円", "40万円", "50万円", "60万円", "70万円", "80万円以上"]
 
     return (
         <div className="flex flex-col justify-center items-center mt-12 gap-4">
-            <div className="mb-8">
+            <div className="mb-5">
                 <img src="../NISAPO_logo.png" alt="logo" className="w-48" />
             </div>
 
-            <div className="label-container">生年月日</div>
-
-            <div className="flex flex-row">
-                <select
-                    className="select select-bordered w-24 max-w-xs mr-1"
-                    value={year}
-                    onChange={(e) => setYear(e.target.value)}
-                >
-                    <option value="" disabled>
-                        年
-                    </option>
-                    <option value="1988">1988</option>
-                    <option value="1989">1989</option>
-                    <option value="1990">1990</option>
-                    <option value="1991">1991</option>
-                </select>
-
-                <select
-                    className="select select-bordered w-24 max-w-xs mr-1"
-                    value={month}
-                    onChange={(e) => setMonth(e.target.value)}
-                >
-                    <option value="" disabled>
-                        月
-                    </option>
-                    {Array.from({ length: 12 }, (_, i) => (
-                        <option key={i + 1} value={i + 1}>
-                            {i + 1}
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">生年月日</div>
+                <div className="grid grid-flow-col justify-stretch mt-2">
+                    <select
+                        className="select select-bordered mx-1"
+                        value={year}
+                        onChange={(e) => setYear(e.target.value)}
+                    >
+                        <option value="" disabled>年</option>
+                        <option value="1988">1988</option>
+                        <option value="1989">1989</option>
+                        <option value="1990">1990</option>
+                        <option value="1991">1991</option>
+                    </select>
+                    <select
+                        className="select select-bordered mx-1"
+                        value={month}
+                        onChange={(e) => setMonth(e.target.value)}
+                    >
+                        <option value="" disabled>
+                            月
                         </option>
-                    ))}
-                </select>
-
-                <select
-                    className="select select-bordered w-24 max-w-xs"
-                    value={day}
-                    onChange={(e) => setDay(e.target.value)}
-                >
-                    <option value="" disabled>
-                        日
-                    </option>
-                    {Array.from({ length: 31 }, (_, i) => (
-                        <option key={i + 1} value={i + 1}>
-                            {i + 1}
+                        {Array.from({ length: 12 }, (_, i) => (
+                            <option key={i + 1} value={i + 1}>
+                                {i + 1}
+                            </option>
+                        ))}
+                    </select>
+                    <select
+                        className="select select-bordered mx-1"
+                        value={day}
+                        onChange={(e) => setDay(e.target.value)}
+                    >
+                        <option value="" disabled>
+                            日
                         </option>
-                    ))}
-                </select>
+                        {Array.from({ length: 31 }, (_, i) => (
+                            <option key={i + 1} value={i + 1}>
+                                {i + 1}
+                            </option>
+                        ))}
+                    </select>
+                </div>
             </div>
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">ご家族</div>
+                <div className="flex flex-col mx-1">
+                    <SelectArea labelList={family_member} n_cols={1}/>
+                </div>
+            </div>
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">ライフプラン</div>
+                <p className='font-bold mt-2'>複数選択可</p>
+                <div className="flex flex-col mx-1">
+                    <MultipleSelectArea labelList={life_plan} n_cols={2}/>
+                </div>
+            </div>
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">現在の資産（貯蓄）</div>
+                <p className='font-bold mt-2'>一番近いものを選択してください</p>
+                <div className="mx-1">
+                    <SelectArea labelList={savings} n_cols={2}/>
+                </div>
+            </div>
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">ご本人様年収</div>
+                <p className='font-bold mt-2'>一番近いものを選択してください</p>
+                <div className="flex flex-col mx-1">
+                    <SelectArea labelList={annual_income} n_cols={2}/>
+                </div>
+            </div>
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">配偶者様年収</div>
+                <p className='font-bold mt-2'>一番近いものを選択してください</p>
+                <div className="flex flex-col mx-1">
+                    <SelectArea labelList={annual_income_spouse} n_cols={2}/>
+                </div>
+            </div>
+            <div className='container mx-auto w-4/5 mb-5'>
+                <div className="label-container">月々の生活費</div>
+                <p className='font-bold mt-2'>一番近いものを選択してください</p>
+                <div className="flex flex-col mx-1">
+                    <SelectArea labelList={monthly_consumption} n_cols={2}/>
+                </div>
+            </div>
+            <button className='btn bg-blue-800 text-white mx-auto w-4/5 mb-5'>結果を見る</button>
 
-            <div className="label-container">ご家族</div>
-            <div className="flex flex-col gap-2">
-                <button
-                    className={`app-selectable-button ${selected === 1 ? 'selected' : ''}`}
-                    onClick={() => handleSelect(1)}
-                >
-                    ご本人
-                </button>
-                <button
-                    className={`app-selectable-button ${selected === 2 ? 'selected' : ''}`}
-                    onClick={() => handleSelect(2)}
-                >
-                    ご夫婦
-                </button>
-                <button
-                    className={`app-selectable-button ${selected === 3 ? 'selected' : ''}`}
-                    onClick={() => handleSelect(3)}
-                >
-                    ご夫婦+お子様1人
-                </button>
-                <button
-                    className={`app-selectable-button ${selected === 4 ? 'selected' : ''}`}
-                    onClick={() => handleSelect(4)}
-                >
-                    ご夫婦+お子様2人以上
-                </button>
-                <button
-                    className={`app-selectable-button ${selected === 5 ? 'selected' : ''}`}
-                    onClick={() => handleSelect(5)}
-                >
-                    親子
-                </button>
-                <button
-                    className={`app-selectable-button ${selected === 6 ? 'selected' : ''}`}
-                    onClick={() => handleSelect(6)}
-                >
-                    ご本人＋ご両親
-                </button>
-            </div>
         </div>
     );
 }
